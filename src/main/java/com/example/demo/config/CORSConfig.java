9bf78c392e3faa7b1458a6da8654bdb6d395ef01
Sebastian Alarcon <sealor92@gmail.com>
2023-10-30 14:52:09 -0500

    se agrega configuración de CORS

---
+++ src/main/java/com/example/demo/config/CORSConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
+    }
+}
